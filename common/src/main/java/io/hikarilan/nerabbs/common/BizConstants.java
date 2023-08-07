1ebaf87ee1cb137bec07b8eea5602343b9da9db1
HikariLan <hikarilan@minecraft.kim>
2023-08-07 11:44:36 +0800

    feature(common): common things

---
+++ common/src/main/java/io/hikarilan/nerabbs/common/BizConstants.java
@@ -0,0 +1,8 @@
+package io.hikarilan.nerabbs.common;
+
+public class BizConstants {
+
+    public static final String USER_ID_HEADER = "X-User-ID";
+    public static final long USER_ID_UNAUTHORIZED = -1L;
+
+}
