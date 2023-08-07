aba26671f5469c2fa024b934a47ac78c45746bc6
HikariLan <hikarilan@minecraft.kim>
2023-08-07 11:39:47 +0800

    sync: dependencies sync

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,11 @@
 allprojects {
     group = "io.hikarilan"
     version = "1.0-SNAPSHOT"
+
+    repositories {
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/apache-snapshots")
+        mavenCentral()
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,11 @@ include("service-auth")
 include("service-user")
 
 include("frontend")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/spring-plugin")
+    }
+}
