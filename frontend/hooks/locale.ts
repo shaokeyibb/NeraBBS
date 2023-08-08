b0802855add0f1a7a94413f70f5027e3e0abb9f3
HikariLan <hikarilan@minecraft.kim>
2023-08-08 09:36:42 +0800

    refactor(frontend): remove redundant setter

--- frontend/hooks/locale.ts
+++ frontend/hooks/locale.ts
@@ -1,12 +1,11 @@
 export default function useLocale() {
-    const {setLocale, setLocaleCookie, messages} = useI18n()
+    const {setLocale, messages} = useI18n()
 
     function getLocalizedLocalName(locale: string) {
         return `${messages.value[locale]['locale.name']} (${messages.value[locale]['locale.intl.name']})`
     }
 
     function setLocalePermanently(locale: string) {
-        setLocaleCookie(locale)
         setLocale(locale)
     }
 
