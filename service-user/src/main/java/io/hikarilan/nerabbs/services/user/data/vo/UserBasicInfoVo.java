481bfd1ade3c3d706e90af37af6cc6e90ebe223a
HikariLan <hikarilan@minecraft.kim>
2023-08-15 15:28:14 +0800

    refactor: remove redundant validation in vo

--- service-user/src/main/java/io/hikarilan/nerabbs/services/user/data/vo/UserBasicInfoVo.java
+++ service-user/src/main/java/io/hikarilan/nerabbs/services/user/data/vo/UserBasicInfoVo.java
@@ -1,16 +1,12 @@
 package io.hikarilan.nerabbs.services.user.data.vo;
 
 import io.hikarilan.nerabbs.services.user.database.entity.UserEntity;
-import jakarta.annotation.Nullable;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.Length;
 
 public record UserBasicInfoVo(long id,
-                              @NotNull @Length(min = 3) @NotBlank String username,
-                              @Nullable @Email String email,
-                              @NotNull String createAt
+                              String username,
+                              String email,
+                              String createAt
 ) {
 
     @NotNull
