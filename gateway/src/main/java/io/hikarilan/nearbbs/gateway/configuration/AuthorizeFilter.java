9697a0861e073600e2beb903435961b8d9dc6584
HikariLan <hikarilan@minecraft.kim>
2025-01-02 23:52:46 +0800

    fix: Sa-Token context

--- gateway/src/main/java/io/hikarilan/nearbbs/gateway/configuration/AuthorizeFilter.java
+++ gateway/src/main/java/io/hikarilan/nearbbs/gateway/configuration/AuthorizeFilter.java
@@ -2,17 +2,20 @@ package io.hikarilan.nearbbs.gateway.configuration;
 
 import cn.dev33.satoken.stp.StpUtil;
 import io.hikarilan.nerabbs.common.BizConstants;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 @Component
-public class AuthorizeFilter implements GlobalFilter {
+public class AuthorizeFilter implements WebFilter {
+
+    @NonNull
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         ServerHttpRequest newRequest = exchange
                 .getRequest()
                 .mutate()
