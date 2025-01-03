f30833567188dc6c1b9ccaaa979873fe80f2fdb6
HikariLan <hikarilan@minecraft.kim>
2025-01-03 15:17:27 +0800

    feat: user status in index page

--- frontend/src/hooks/auth.ts
+++ frontend/src/hooks/auth.ts
@@ -1,7 +1,8 @@
 import useBackend from "./backend.ts";
+import type { ErrorMessage } from "../types/error-handling.ts";
 
 export default function useAuth() {
-  const { _signIn, _signUp, _signOut } = useBackend();
+  const { _signIn, _signUp, _signOut, _getUserInfo } = useBackend();
 
   const signIn = async (email: string, password: string) => {
     await _signIn(email, password);
@@ -15,9 +16,23 @@ export default function useAuth() {
     await _signOut();
   };
 
+  const isLoggedIn = async () => {
+    try {
+      await _getUserInfo();
+      return true;
+    } catch (e) {
+      const err = e as ErrorMessage;
+      if (err.code == 401) {
+        return false;
+      }
+      throw e;
+    }
+  };
+
   return {
     signIn,
     signUp,
     signOut,
+    isLoggedIn,
   };
 }
